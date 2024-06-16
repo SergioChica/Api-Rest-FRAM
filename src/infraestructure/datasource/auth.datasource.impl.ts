cbc2de710a50120f86a93e60d0460294955a90db
Sergio Chica <sergioanjaci@gmail.com>
2024-06-16 15:55:55 -0500

    Implementacion mapper para el objeto de entidades

--- src/infraestructure/datasource/auth.datasource.impl.ts
+++ src/infraestructure/datasource/auth.datasource.impl.ts
@@ -6,6 +6,7 @@ import {
   RegisterUserDto,
   UserEntity,
 } from "../../domain"; //2
+import { UserMapper } from "../mappers/user.mapper";
 
 // creamos types de los metodos de encriptacion
 
@@ -44,13 +45,7 @@ export class AuthDataSourceImpl implements AuthDataSource {
       // TODO: Hash de contraseña
 
       // TODO: Mapear la respuesta a nuestra entidad
-      return new UserEntity( // 6
-        user.id, // se reemplazó '1'
-        name,
-        email,
-        user.password,//se reemplazó por password
-        user.roles //se reemplazó ['USER']
-      );
+      return UserMapper.userEntityFromObject(user)
     } catch (error) {
       if (error instanceof CustomError) {
         //7
